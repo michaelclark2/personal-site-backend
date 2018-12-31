8380d4563e701f0a4992c1f2ddb896efea4f7a69
michael <michael.clark1992@gmail.com>
2018-12-30 20:51:12 -0600

    added old site data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,11 @@
-Blog.create({:title => 'Blog Post Title', :body => 'A long ass string of text in order to test this.'})
-Project.create({:title => 'Project x', :desc => 'this is project x.  it does stuff', :techs => 'all the technologies', :github => 'https://github.com/michaelclark2', :url => 'https://michaelclark.co'})
+require 'json'
+
+seeds = JSON.parse(File.readlines('./db/seeds.json').join)
+
+seeds['projects'].each do |project|
+  Project.create project
+end
+
+seeds['blogs'].each do |blog|
+  Blog.create blog
+end
