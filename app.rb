3faa365ee679f4413bfe0d1b491d42b9925f111d
michael <michael.clark1992@gmail.com>
2018-12-30 19:41:57 -0600

    gets all routes in app on index

--- app.rb
+++ app.rb
@@ -1,8 +1,20 @@
 require 'sinatra'
 require 'json'
 
+
 class App < Sinatra::Base
-  get '/' do
-    'Hello, world!'
+
+
+  get '/aye' do
+    'Aye lmao'
   end
+
+  get '/' do
+    routes = []
+    self.class.routes["GET"].each do |route|
+      routes.push route[0].to_s
+    end
+    routes.to_json
+  end
+
 end
